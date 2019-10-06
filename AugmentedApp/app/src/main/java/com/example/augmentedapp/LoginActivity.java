2d28b8f612024b71e6d5f9ea2729bb04266a5f58
Tahmid1406 <tahmidpranto1406@gmail.com>
2019-10-07 01:31:49 +0600

    Login With mail Added(FireBase Authentication)

--- AugmentedApp/app/src/main/java/com/example/augmentedapp/LoginActivity.java
+++ AugmentedApp/app/src/main/java/com/example/augmentedapp/LoginActivity.java
@@ -1,25 +1,80 @@
 package com.example.augmentedapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    GoogleSignInClient mGoogleSignInClient;
     TextView signUpTextView;
+    private FirebaseAuth mAuth;
+    CallbackManager mCallbackManager;
+    Button loginButton,buttonGoogle;
+    private CallbackManager callbackManager;
+    EditText emailEditText,passwordEditText;
 
     public void signUpButtonPressed(View view){
-        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
+    }
+
+
+    public void onLoginButtonPressed(View view) {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "User Exists", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    public void onGoogleButtonPressed(View view){
+        Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        signUpTextView = (TextView) findViewById(R.id.signUpButton);
+        signUpTextView = (TextView) findViewById(R.id.signUpTextView);
+        emailEditText = (EditText)findViewById(R.id.emailEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        mAuth = FirebaseAuth.getInstance();
+        loginButton = (Button) findViewById(R.id.loginButton);
+        buttonGoogle = (Button) findViewById(R.id.buttonGoogle);
+
+
 
 
     }
--- AugmentedApp/app/src/main/java/com/example/augmentedapp/MainActivity.java
+++ AugmentedApp/app/src/main/java/com/example/augmentedapp/MainActivity.java
@@ -1,17 +1,58 @@
 package com.example.augmentedapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
 
+    private FirebaseAuth mAuth;
+    TextView textView;
+    EditText emailEditText,passwordEditText;
+    Button signUpButton;
+    //signUp button on click function
+
+    public void onSignUpButtonClick(View view){
+        String mail = emailEditText.getText().toString();
+        String pass = passwordEditText.getText().toString();
+        if(TextUtils.isEmpty(mail) && TextUtils.isEmpty(pass)){
+            Toast.makeText(this, "Email Or password Empty", Toast.LENGTH_SHORT).show();
+        }else if(!TextUtils.isEmpty(mail) && !TextUtils.isEmpty(pass)){
+            mAuth.createUserWithEmailAndPassword(mail,pass).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Invalid User Or Password", Toast.LENGTH_SHORT).show();
+                    }else{
+                        startActivity(new Intent(MainActivity.this,HomeActivity.class));
+                    }
+                }
+            });
+        }else{
+            Toast.makeText(this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+
+    //login textView on CLick Function
     public void loginViewPressed(View view){
-        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
     }
 
@@ -19,6 +60,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUpButton = (Button) findViewById(R.id.loginButton);
+        emailEditText = (EditText)findViewById(R.id.emailEditText);
+        passwordEditText = (EditText)findViewById(R.id.passwordEditText);
         textView = (TextView) findViewById(R.id.signUpTextView);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
     }
 }
