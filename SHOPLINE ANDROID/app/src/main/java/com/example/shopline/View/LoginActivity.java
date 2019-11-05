624bb04ea65fa0e845e74327d0778321409950a5
Tahmid1406 <tahmidpranto1406@gmail.com>
2019-11-05 18:31:00 +0600

    signUp activity added

--- SHOPLINE ANDROID/app/src/main/java/com/example/shopline/View/LoginActivity.java
+++ SHOPLINE ANDROID/app/src/main/java/com/example/shopline/View/LoginActivity.java
@@ -18,50 +18,7 @@ import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    TextView textView;
-    EditText emailEditText,passwordEditText;
-    Button signUpButton,continueButton;
-    //signUp button on click function
-
-    public void onSignUpButtonClick(View view){
-        String mail = emailEditText.getText().toString();
-        String pass = passwordEditText.getText().toString();
-        if(TextUtils.isEmpty(mail) && TextUtils.isEmpty(pass)){
-            Toast.makeText(this, "Email Or password Empty", Toast.LENGTH_SHORT).show();
-        }else if(!TextUtils.isEmpty(mail) && !TextUtils.isEmpty(pass)){
-            mAuth.createUserWithEmailAndPassword(mail,pass).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(!task.isSuccessful()){
-                        Toast.makeText(MainActivity.this, "Invalid User Or Password", Toast.LENGTH_SHORT).show();
-                    }else{
-                        startActivity(new Intent(MainActivity.this,HomeActivity.class));
-                    }
-                }
-            });
-        }else{
-            Toast.makeText(this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    //continue button OnClick Func
-    public void onContinueButtonClick(View view){
-        Intent intent = new Intent(MainActivity.this,HomeActivity.class);
-        startActivity(intent);
-    }
-
-
-
-
-
-    //login textView on CLick Function
-    public void loginViewPressed(View view){
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
-    }
-
-
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- SHOPLINE ANDROID/app/src/main/java/com/example/shopline/View/MainActivity.java
+++ SHOPLINE ANDROID/app/src/main/java/com/example/shopline/View/MainActivity.java
@@ -23,44 +23,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.text_result);
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://shopline-29362.firebaseio.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        JsonPlaceholderAPI jsonPlaceholderAPI = retrofit.create(com.example.shopline.Controller.JsonPlaceholderAPI.class);
-
-        Call<List<Post>> call = jsonPlaceholderAPI.getPosts();
-
-        call.enqueue(new Callback<List<Post>>() {
-            @Override
-            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-                if(!response.isSuccessful()){
-                    textView.setText(response.code());
-                    return;
-                }
-                List<Post> posts = response.body();
-                for (Post x : posts){
-                    String content = "";
-                    content += "ID: " + x.getId()+"\n";
-                    content+= "User Id : " + x.getUserId()+"\n";
-                    content += "Name: " +x.getName() +"\n";
-                    content += "Info: " + x.getText()+ "\n\n";
-                    textView.append(content);
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<List<Post>> call, Throwable t) {
-                textView.setText(t.getMessage());
-            }
-        });
-
-
-
-
     }
 }
