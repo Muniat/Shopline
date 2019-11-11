19306a3902e3896f85f7860c1a88854dc4b62a50
Tahmid1406 <tahmidpranto1406@gmail.com>
2019-11-12 01:54:51 +0600

    botton nav fuctionality improved

--- SHOPLINE ANDROID/UserRegistration/app/src/main/java/com/example/userregistration/HomeActivity.java
+++ SHOPLINE ANDROID/UserRegistration/app/src/main/java/com/example/userregistration/HomeActivity.java
@@ -1,23 +1,71 @@
 package com.example.userregistration;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.userregistration.Fragments.AccountFragment;
+import com.example.userregistration.Fragments.CartFragment;
+import com.example.userregistration.Fragments.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     TextView logOutTextView;
+    FrameLayout mframeLayout;
+    BottomNavigationView mbottomNavigationView;
+    HomeFragment homeFragment;
+    AccountFragment accountFragment;
+    CartFragment cartFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        logOutTextView = (TextView) findViewById(R.id.logOutTextView);
+        homeFragment = new HomeFragment();
+        cartFragment = new CartFragment();
+        accountFragment = new AccountFragment();
+        addFragment(homeFragment);
+
+        mframeLayout = (FrameLayout) findViewById(R.id.mainFrame);
+        mbottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
+
+        mbottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.homeNav :
+                        Toast.makeText(HomeActivity.this, "Home Pressed", Toast.LENGTH_SHORT).show();
+                        addFragment(homeFragment);
+                        return true;
+                    case R.id.accountNav:
+                        Toast.makeText(HomeActivity.this, "Account", Toast.LENGTH_SHORT).show();
+                        addFragment(accountFragment);
+                        return true;
+                    case R.id.cartNav:
+                        Toast.makeText(HomeActivity.this, "cart", Toast.LENGTH_SHORT).show();
+                        addFragment(cartFragment);
+                        return  true;
+                        default:
+                            return false;
+                }
+            }
+        });
+
+
+
+
+
+
+       /* logOutTextView = (TextView) findViewById(R.id.logOutTextView);
 
         logOutTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +74,12 @@ public class HomeActivity extends AppCompatActivity {
                 Intent intent = new Intent(HomeActivity.this, MainActivity.class);
                 startActivity(intent);
             }
-        });
+        }); */
+    }
+
+    private void addFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainFrame,fragment);
+        fragmentTransaction.commit();
     }
 }
