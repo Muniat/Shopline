ad3a63482c65170ca207fd74f792edf107265be3
Tahmid1406 <tahmidpranto1406@gmail.com>
2019-12-14 01:43:40 +0600

    accounts and userProfile added

--- SHOPLINE ANDROID/UserRegistration/app/src/main/java/com/example/userregistration/Fragments/AccountFragment.java
+++ SHOPLINE ANDROID/UserRegistration/app/src/main/java/com/example/userregistration/Fragments/AccountFragment.java
@@ -3,18 +3,27 @@ package com.example.userregistration.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.userregistration.R;
+import com.example.userregistration.prevalent.Prevalent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AccountFragment extends Fragment {
+    TextView userEmail,userPhone;
 
 
     public AccountFragment() {
@@ -26,7 +35,35 @@ public class AccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View rootview =  inflater.inflate(R.layout.fragment_account, container, false);
+        userEmail = (TextView) rootview.findViewById(R.id.userEmail);
+        userPhone = (TextView) rootview.findViewById(R.id.userPhone);
+
+        setUserInfo();
+
+
+        return rootview;
+    }
+
+    private void setUserInfo() {
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("users")
+                .child(Prevalent.currentOnlineUser.getPhone());
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    String mail = dataSnapshot.child("email").getValue().toString();
+                    String phone = dataSnapshot.child("phone").getValue().toString();
+                    userEmail.setText("Email : " + mail);
+                    userPhone.setText("Phone No. : " + phone);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
--- SHOPLINE ANDROID/UserRegistration/app/src/main/java/com/example/userregistration/Fragments/SettingFragment.java
+++ SHOPLINE ANDROID/UserRegistration/app/src/main/java/com/example/userregistration/Fragments/SettingFragment.java
@@ -3,18 +3,32 @@ package com.example.userregistration.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.userregistration.R;
+import com.example.userregistration.prevalent.Prevalent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SettingFragment extends Fragment {
+    ImageView profilePicture;
+    TextView changeImageTextview,updateSettings,closeSettings;
+    EditText changeNumber,fullName,fullAddress,changeEmail;
 
 
     public SettingFragment() {
@@ -26,7 +40,20 @@ public class SettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View rootview =  inflater.inflate(R.layout.fragment_setting, container, false);
+        profilePicture = (ImageView) rootview.findViewById(R.id.profilePicture);
+        changeImageTextview = (TextView) rootview.findViewById(R.id.changeImageTextview);
+        updateSettings = (TextView) rootview.findViewById(R.id.updateSettings);
+        closeSettings = (TextView) rootview.findViewById(R.id.closeSettings);
+        changeNumber = (EditText) rootview.findViewById(R.id.changeNumber);
+        fullName = (EditText) rootview.findViewById(R.id.fullName);
+        fullAddress = (EditText) rootview.findViewById(R.id.fullAddress);
+        changeEmail = (EditText) rootview.findViewById(R.id.changeEmail);
+        
+
+
+
+        return rootview;
     }
 
 }
