3ad802d196a17c87b63802f065904787e122a78c
Tahmid1406 <tahmidpranto1406@gmail.com>
2019-12-15 22:19:40 +0600

    user now can update their profile :wq

--- SHOPLINE ANDROID/UserRegistration/app/src/main/java/com/example/userregistration/Fragments/SettingFragment.java
+++ SHOPLINE ANDROID/UserRegistration/app/src/main/java/com/example/userregistration/Fragments/SettingFragment.java
@@ -1,6 +1,8 @@
 package com.example.userregistration.Fragments;
 
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,9 +14,13 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.userregistration.R;
+import com.example.userregistration.View.HomeActivity;
 import com.example.userregistration.prevalent.Prevalent;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,13 +28,16 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.HashMap;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SettingFragment extends Fragment {
     ImageView profilePicture;
     TextView changeImageTextview,updateSettings,closeSettings;
-    EditText changeNumber,fullName,fullAddress,changeEmail;
+    EditText changeNumber,changeEmail;
+    public ProgressDialog loadingBar;
 
 
     public SettingFragment() {
@@ -46,14 +55,89 @@ public class SettingFragment extends Fragment {
         updateSettings = (TextView) rootview.findViewById(R.id.updateSettings);
         closeSettings = (TextView) rootview.findViewById(R.id.closeSettings);
         changeNumber = (EditText) rootview.findViewById(R.id.changeNumber);
-        fullName = (EditText) rootview.findViewById(R.id.fullName);
-        fullAddress = (EditText) rootview.findViewById(R.id.fullAddress);
         changeEmail = (EditText) rootview.findViewById(R.id.changeEmail);
         
+        setUserInfo();
 
+        updateSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingBar.setTitle("Login Account");
+                loadingBar.setMessage("Please Wait");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
+                String mail = changeEmail.getText().toString();
+                String phone = changeNumber.getText().toString();
+                DatabaseReference updateRef = FirebaseDatabase.getInstance().getReference().child("users").child(Prevalent.currentOnlineUser.getPhone());
+                updateRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String snapMail = dataSnapshot.child("email").getValue().toString();
+                        String snapPhone = dataSnapshot.child("phone").getValue().toString();
+                        String snapPass = dataSnapshot.child("password").getValue().toString();
+                        if(mail == snapMail && phone== snapPhone){
+                            Toast.makeText(SettingFragment.this.getContext(), "No change in email or password", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+                        }else{
+                            HashMap<Object, String> updateMap = new HashMap<>();
+                            updateMap.put("email", mail);
+                            updateMap.put("phone", phone);
+                            updateMap.put("password", snapPass);
+                            updateRef.setValue(updateMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        Toast.makeText(SettingFragment.this.getContext(), "Account Information Updated", Toast.LENGTH_SHORT).show();
+                                        loadingBar.dismiss();
+                                    }else{
+                                        Toast.makeText(SettingFragment.this.getContext(), "Update Failed ! ", Toast.LENGTH_SHORT).show();
+                                        loadingBar.dismiss();
+                                    }
+                                }
+                            });
+                        }
+                    }
 
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        closeSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingFragment.this.getContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return rootview;
+    }
+
+
+
+    private void setUserInfo() {
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("users")
+                .child(Prevalent.currentOnlineUser.getPhone());
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    String mail = dataSnapshot.child("email").getValue().toString();
+                    String phone = dataSnapshot.child("phone").getValue().toString();
+                    changeEmail.setText(mail);
+                    changeNumber.setText(phone);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
